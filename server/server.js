5f1f032036fc0343d4c6d1443d13398d34ebb38f
leandroblanco456 <leandro.blanco@alumnos.frm.utn.edu.ar>
2020-07-07 12:08:14 -0300

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,59 @@
+
+
+//archivos locales
+ require('./config/config');
+
+
+// paquetes exteriores
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', function (req, res) {
+    res.json('Get usuario')
+})
+app.post('/usuario', function (req, res) {
+
+    let body = req.body;
+
+    if ( body.nombre === undefined ){
+        res.status(400).json({
+            ok: false,
+            mensaje: 'el nombre es necesario'
+        });
+
+    } else {
+        res.json({
+            persona: body
+        })
+    }
+
+    res.json({
+        body
+    })
+})
+app.put('/usuario/:id', function (req, res) {
+
+    let id = req.params.id
+
+    res.json({
+        id,
+
+    })
+})
+app.delete('/usuario', function (req, res) {
+    res.json('delete usuario')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('escuchando el puerto', process.env.PORT);
+})
