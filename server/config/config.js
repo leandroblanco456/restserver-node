481f04bd24258419056f76ac983761438cea0066
leandroblanco456 <leandro.blanco@alumnos.frm.utn.edu.ar>
2020-07-14 12:45:27 -0300

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -32,7 +32,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://lean456:BUfJX7zBVMAZ53zH@cluster0.lg4mc.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
